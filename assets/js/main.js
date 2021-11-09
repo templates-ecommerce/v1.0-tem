a7248a6131a5fdba9806b150e7304692ae181ae2
amofficial888 <amofficial888@gmail.com>
2021-11-09 16:21:04 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1208,40 +1208,40 @@ function ecCheckCookie()
     });
 
     /*----------------------------- Slider Price -------------------------------- */
-    const slider = document.getElementById('ec-sliderPrice');
-    if(slider){
-        const rangeMin = parseInt(slider.dataset.min);
-        const rangeMax = parseInt(slider.dataset.max);
-        const step = parseInt(slider.dataset.step);
-        const filterInputs = document.querySelectorAll('input.filter__input');
+    // const slider = document.getElementById('ec-sliderPrice');
+    // if(slider){
+    //     const rangeMin = parseInt(slider.dataset.min);
+    //     const rangeMax = parseInt(slider.dataset.max);
+    //     const step = parseInt(slider.dataset.step);
+    //     const filterInputs = document.querySelectorAll('input.filter__input');
 
-        noUiSlider.create(slider, {
-            start: [rangeMin, rangeMax],
-            connect: true,
-            step: step,
-            range: {
-                'min': rangeMin,
-                'max': rangeMax
-            },
+    //     noUiSlider.create(slider, {
+    //         start: [rangeMin, rangeMax],
+    //         connect: true,
+    //         step: step,
+    //         range: {
+    //             'min': rangeMin,
+    //             'max': rangeMax
+    //         },
 
-            // make numbers whole
-            format: {
-                to: value => value,
-                from: value => value
-            }
-        });
+    //         // make numbers whole
+    //         format: {
+    //             to: value => value,
+    //             from: value => value
+    //         }
+    //     });
 
-        // bind inputs with noUiSlider 
-        slider.noUiSlider.on('update', (values, handle) => {
-            filterInputs[handle].value = values[handle];
-        });
+    //     // bind inputs with noUiSlider 
+    //     slider.noUiSlider.on('update', (values, handle) => {
+    //         filterInputs[handle].value = values[handle];
+    //     });
 
-        filterInputs.forEach((input, indexInput) => {
-            input.addEventListener('change', () => {
-                slider.noUiSlider.setHandle(indexInput, input.value);
-            })
-        });
-    }
+    //     filterInputs.forEach((input, indexInput) => {
+    //         input.addEventListener('change', () => {
+    //             slider.noUiSlider.setHandle(indexInput, input.value);
+    //         })
+    //     });
+    // }
     
     /*----------------------------- Cart Page Qty Plus Minus Button  ------------------------------ */
     var CartQtyPlusMinus = $(".cart-qty-plus-minus");
